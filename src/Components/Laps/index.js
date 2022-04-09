0e0b373dd3a3fea7ccf1c8e29aa54f00386825cb
Hoang Anh <anh.hoang@employmenthero.com>
2022-04-09 11:22:20 +0700

    create layout

---
+++ src/Components/Laps/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Box, Text } from "@chakra-ui/react";
+
+const BaseLapData = () => (
+  <Box>
+    <Text fontSize="2xl">Laps</Text>
+  </Box>
+);
+
+export default BaseLapData;
